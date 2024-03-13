33fb873cfb036eb94d581ff90b7f45d5c12c4da9
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-03-13 14:50:38 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = int(environ.get("API_ID", "23941369"))
+    API_HASH = environ.get("API_HASH", "bbd37235e41a95d03bc144ea6bc7b2cd")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://ROKU:ROKU@cluster0.nxjre0s.mongodb.net/?retryWrites=true&w=majority")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Fzxforwardbot")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
     
 class temp(object): 
