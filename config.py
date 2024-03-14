f312df3dad23ec97052ec8af063286fa9a1077ac
ARRIPAPPA <137713659+ARRIPAPPA@users.noreply.github.com>
2024-03-14 21:18:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://ROKU:ROKU@cluster0.nxjre0s.mongodb.net/?retryWrites=true&w=majority")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Fzxforwardbot")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '5422016608').split()]
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '5422016608 6748415360').split()]
     
 class temp(object): 
     lock = {}
