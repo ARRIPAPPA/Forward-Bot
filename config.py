a538b8d6f5e80436d8973bcbacd4c925b9998b57
ARRIPAPPA <137713659+ARRIPAPPA@users.noreply.github.com>
2024-03-14 21:23:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     API_HASH = environ.get("API_HASH", "bbd37235e41a95d03bc144ea6bc7b2cd")
     BOT_TOKEN = environ.get("BOT_TOKEN", "6523875009:AAF9AZEmYOiG4u2HuetP3YtGrIjLhudzVoQ") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://ROKU:ROKU@cluster0.nxjre0s.mongodb.net/?retryWrites=true&w=majority")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://Forward:Forward@cluster0.2kpl6qb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Fzxforwardbot")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '5422016608 6748415360').split()]
     
